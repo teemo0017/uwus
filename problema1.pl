f70a619bf781a85f2f411ae7d5843d39ba98d90c
arcontreras <arcontreras@bgeneral.com>
2023-05-21 22:38:03 -0500

    TALLER4

--- problema1.pl
+++ problema1.pl
@@ -1,16 +1,17 @@
 menu:-
-	write('Digita 1 para sumar'),nl,
-	write('Digita 2 para restar '),nl,
-	write('Digita 3 para multiplicar'),nl,
-	write('Digita 4 para dividir '),nl,
+	write('Digita el numero que deseas saber su factorial'),nl,
 	read(Opcion),
-	write('Ingrese primer digito'),nl,
-	read(N1),
-	write('Ingrese segundo digito  '),nl,
-	read(N2),
-	ejecutar(Opcion,N1,N2).
-	ejecutar(Opcion,N1,N2) :-   Opcion==1,SUMA is N1+N2,write(SUMA);
-		                   Opcion==2,RESTA is N1-N2,write(RESTA);
-			Opcion==3,MULT is N1*N2,write(MULT);
-			Opcion==4,DIV is N1/N2,write(DIV);
+	ejecutar(Opcion).
+	ejecutar(Opcion) :-   Opcion==2,FACTORIAL is 1*2,write(FACTORIAL);
+						  Opcion==3,FACTORIAL is 1*2*3,write(FACTORIAL);
+						  Opcion==4,FACTORIAL is 1*2*3*4,write(FACTORIAL);
+						  Opcion==5,FACTORIAL is 1*2*3*4*5,write(FACTORIAL);
+						  Opcion==6,FACTORIAL is 1*2*3*4*5*6,write(FACTORIAL);
+						  Opcion==7,FACTORIAL is 1*2*3*4*5*6*7,write(FACTORIAL);
+						  Opcion==8,FACTORIAL is 1*2*3*4*5*6*7*8,write(FACTORIAL);
+						  Opcion==9,FACTORIAL is 1*2*3*4*5*6*7*8*9,write(FACTORIAL);
+						  Opcion==10,FACTORIAL is 1*2*3*4*5*6*7*8*9*10,write(FACTORIAL);
+						  Opcion==11,FACTORIAL is 1*2*3*4*5*6*7*8*9*10*11,write(FACTORIAL);
+						  Opcion==12,FACTORIAL is 1*2*3*4*5*6*7*8*9*10*12,write(FACTORIAL);
+
 	write('Opcion no estipulada').
--- problema2.pl
+++ problema2.pl
@@ -1,11 +1,12 @@
 menu:-
-	write('Hotel Arodi'),nl,
-	write('Ingresa la cantidad de dias de hospedeje '),nl,
+	write('Digita el numero que deseas saber sucesion fibonacci'),nl,
 	read(Opcion),
 	ejecutar(Opcion).
-
-	ejecutar(Opcion) :-   Opcion==1,write('HOLA');
-		                   Opcion==2,T is 95.00*2 ,write('Precio de Estadia B/.'+T),nl,write('Total de Importe B/.'+T);
-			Opcion>=3,T is 95.00*Opcion ,D is T*0.3, TD is T-D,write('Precio de Estadia B/.' +T),nl,write('Descuento del 30% B/.'+D),nl,write('Total de Importe B/.'+TD);
-			
+	ejecutar(Opcion) :-   Opcion==1,FIBO is 0+1,write(FIBO);
+						  Opcion==2,FIBO is 1+1,write(FIBO);
+						  Opcion==3,FIBO is 2+1,write(FIBO);
+						  Opcion==4,FIBO is 3+2,write(FIBO);
+						  Opcion==5,FIBO is 5+3,write(FIBO);
+						  Opcion==6,FIBO is 8+5,write(FIBO);
+						  Opcion==7,FIBO is 13+8,write(FIBO);
 	write('Opcion no estipulada').
